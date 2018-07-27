9016cf38e73f3c2cae5b765e70a6208b5303b6fc
ch <ch@7e2f.org>
2018-07-27 08:26:17 -0700

    update

--- app/src/main/java/org/ch/launcher/HomeScreen.java
+++ app/src/main/java/org/ch/launcher/HomeScreen.java
@@ -1,22 +1,23 @@
 package org.ch.launcher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.view.View;
 
-public class HomeScreen extends FragmentActivity {
+public class HomeScreen extends FragmentActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.homescreen);
+        setContentView(R.layout.custom);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
-        return true;
+    public void onClick(View v) {
+        String pkg = v.getTag().toString();
+        Intent intent = getPackageManager().getLaunchIntentForPackage(pkg);
+        startActivity(intent);
+
     }
 }
