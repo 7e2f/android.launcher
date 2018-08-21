c4efc03e6e1da435e8ec1355592a3216a5814b46
ch <ch@7e2f.org>
2018-08-21 02:30:49 -0400

    update

--- app/src/main/java/org/ch/launcher/HomeScreen.java
+++ app/src/main/java/org/ch/launcher/HomeScreen.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.Window;
 
 public class HomeScreen extends FragmentActivity implements View.OnClickListener {
 
@@ -11,6 +13,8 @@ public class HomeScreen extends FragmentActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
     }
 
     @Override
@@ -18,6 +22,5 @@ public class HomeScreen extends FragmentActivity implements View.OnClickListener
         String pkg = v.getTag().toString();
         Intent intent = getPackageManager().getLaunchIntentForPackage(pkg);
         startActivity(intent);
-
     }
 }
